603d27b41b3091ea06c0399d2d30fe5c2168bf11
bxbx4mzz <beam.wipawee2548@gmail.com>
2026-09-13 19:25:50 +0700

    set up java project

---
+++ rental/src/main/java/com/g10/rental/HomeController.java
@@ -0,0 +1,13 @@
+package com.g10.rental;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to the Rental Application!";
+    }
+}
